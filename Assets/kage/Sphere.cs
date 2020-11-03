fda9c69dcc03a56961840b37eb3b8a641581b92b
kage1020 <4620087@ed.tus.ac.jp>
2020-11-03 19:08:05 +0900

    v1103

---
+++ Assets/kage/Sphere.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class Sphere : MonoBehaviour
+{
+    public Rigidbody rigidbody;
+    public Vector3 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody.AddForce(velocity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
